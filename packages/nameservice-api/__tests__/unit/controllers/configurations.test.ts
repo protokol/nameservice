d50d7a039700c14ba0bae9ebfe3b5707c30f5486
KovacZan <zan@protokol.com>
2021-08-15 10:31:41 +0200

    refactor(nameservice-api): refactor configurations endpoint

--- packages/nameservice-api/__tests__/unit/controllers/configurations.test.ts
+++ packages/nameservice-api/__tests__/unit/controllers/configurations.test.ts
@@ -7,6 +7,7 @@ import { Defaults as CryptoDefaults } from "@protokol/nameservice-crypto";
 import { Defaults as TransactionsDefaults } from "@protokol/nameservice-transactions";
 import latestVersion from "latest-version";
 
+import { defaults } from "../../../src";
 import { ConfigurationController } from "../../../src/controllers/configurations";
 import { initApp, ItemResponse } from "../__support__";
 
@@ -29,13 +30,24 @@ describe("Test configurations controller", () => {
         const plugin = require("../../../package.json");
 
         expect(response.data).toStrictEqual({
-            package: {
-                name: plugin.name,
+            api: {
+                packageName: plugin.name,
                 currentVersion: plugin.version,
                 latestVersion: await latestVersion(plugin.name),
+                defaults: defaults,
             },
-            crypto: CryptoDefaults,
-            transactions: TransactionsDefaults,
+            transactions: {
+                packageName: "@protokol/nameservice-transactions",
+                currentVersion: plugin.version,
+                latestVersion: await latestVersion("@protokol/nameservice-transactions"),
+                defaults: TransactionsDefaults,
+            },
+            crypto: {
+                packageName: "@protokol/nameservice-crypto",
+                currentVersion: plugin.version,
+                latestVersion: await latestVersion("@protokol/nameservice-crypto"),
+                defaults: CryptoDefaults,
+            },
         });
     });
 });
--- packages/nameservice-api/src/controllers/configurations.ts
+++ packages/nameservice-api/src/controllers/configurations.ts
@@ -5,6 +5,7 @@ import { Defaults as CryptoDefaults } from "@protokol/nameservice-crypto";
 import { Defaults as TransactionDefaults } from "@protokol/nameservice-transactions";
 import latestVersion from "latest-version";
 
+import { defaults as ApiDefaults } from "../defaults";
 import { ConfigurationResource } from "../resources/configurations";
 
 const packageName = require("../../package.json").name;
@@ -12,14 +13,21 @@ const currentVersion = require("../../package.json").version;
 
 @Container.injectable()
 export class ConfigurationController extends Controller {
-    public async index(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+    public async index(request: Hapi.Request, h: Hapi.ResponseToolkit): Promise<any> {
+        const apiLatestVersion = await latestVersion(packageName);
+        const cryptoLatestVersion = await latestVersion("@protokol/nameservice-crypto");
+        const transactionsLatestVersion = await latestVersion("@protokol/nameservice-transactions");
+
         return this.respondWithResource(
             {
-                packageName,
-                currentVersion,
-                latestVersion: await latestVersion(packageName),
-                crypto: CryptoDefaults,
-                transactions: TransactionDefaults,
+                apiPackageName: packageName,
+                apiLatestVersion,
+                cryptoLatestVersion,
+                transactionsLatestVersion,
+                currentVersion: currentVersion,
+                transactionsDefaults: TransactionDefaults,
+                cryptoDefaults: CryptoDefaults,
+                apiDefaults: ApiDefaults,
             },
             ConfigurationResource,
         );
