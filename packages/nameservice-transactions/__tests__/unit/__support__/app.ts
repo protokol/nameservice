a0637698c7065fbdfc25609d81eef06e65d22e4a
KovacZan <zan@protokol.com>
2021-09-22 11:34:47 +0200

    fix(nameservice-transactions): fix unit tests

--- packages/nameservice-transactions/__tests__/unit/__support__/app.ts
+++ packages/nameservice-transactions/__tests__/unit/__support__/app.ts
@@ -15,6 +15,7 @@ import {
     VerifyTransactionAction,
 } from "@arkecosystem/core-transaction-pool";
 import { Handlers } from "@arkecosystem/core-transactions";
+import { ServiceProvider } from "@arkecosystem/core-transactions";
 import { Identities, Managers, Utils } from "@arkecosystem/crypto";
 
 import { NameserviceTransactionHandler } from "../../../src/handlers";
@@ -126,6 +127,9 @@ export const initApp = (): Application => {
         .to(Handlers.TransactionHandlerProvider)
         .inSingletonScope();
     app.bind(Container.Identifiers.TransactionHandlerRegistry).to(Handlers.Registry).inSingletonScope();
+    app.bind(Container.Identifiers.TransactionHandlerConstructors).toDynamicValue(
+        ServiceProvider.getTransactionHandlerConstructorsBinding(),
+    );
 
     app.bind(Container.Identifiers.TriggerService).to(Services.Triggers.Triggers).inSingletonScope();
 
